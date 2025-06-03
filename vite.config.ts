bca6234801d789a3c9b0faebfbe917a8ead49b93
keolia <126954519+keolia@users.noreply.github.com>
2025-06-03 14:13:33 +0200

    Fix HMR WebSocket Connection

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true,
-    hmr: {
-      host: 'localhost'
-    }
+    host: true
   },
   preview: {
     port: 5173,
